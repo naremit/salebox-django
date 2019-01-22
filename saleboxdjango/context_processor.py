327773ccafd39ad71c9245e037ad7e2c808ddfa2
Jon Combe <github@joncombe.net>
2019-01-22 08:45:47 +0700

    Wagtail preview bug fixed

--- saleboxdjango/context_processor.py
+++ saleboxdjango/context_processor.py
@@ -1,5 +1,22 @@
 def salebox(request):
-    return {
-        'basket': request.session['basket'],
-        'user': request.user
-    }
+    try:  # used to prevent issues with wagtail preview
+        return {
+            'basket': request.session['basket'],
+            'user': request.user
+        }
+    except:
+        return {
+            'basket': {
+                'basket': {
+                    'quantity': 0,
+                    'loyalty': 0,
+                    'price': 0,
+                    'contents': {},
+                },
+                'wishlist': {
+                    'quantity': 0,
+                    'contents': [],
+                }
+            },
+            'user': None
+        }
