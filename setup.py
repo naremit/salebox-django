4f93830a7da7b03dbf08a9aca6abfe7651230a7c
Jon Combe <github@joncombe.net>
2019-02-04 11:11:53 +0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='salebox-django',
-    version='0.0.5',
+    version='0.0.6',
     author=u'Jon Combe',
     author_email='jon@getsalebox.com',
     packages=find_packages(),
