ec5ab8ab8b7c33044faa887894acc1872306d792
Jon Combe <github@joncombe.net>
2019-03-19 13:32:00 +0700

    Views reorg progress

--- saleboxdjango/urls.py
+++ saleboxdjango/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from saleboxdjango.views import *
+from saleboxdjango.views.address import addresslist_ajax_view
+from saleboxdjango.views.basket import basket_ajax_view, \
+    switch_basket_wishlist_ajax_view, wishlist_ajax_view
+from saleboxdjango.views.image import image_view
+from saleboxdjango.views.rating import rating_ajax_view
 
 urlpatterns = [
     path('delivery-address-list/', addresslist_ajax_view),
