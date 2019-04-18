2400e68d85ad097493861c175e585b89c7d36b1f
Jon Combe <github@joncombe.net>
2019-04-18 15:17:31 +0700

    i18n progress

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='salebox-django',
-    version='0.0.82',
+    version='0.0.84',
     author=u'Jon Combe',
     author_email='jon@getsalebox.com',
     packages=find_packages(),
