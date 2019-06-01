f86b98a2658c2acccc1593d84a0bd0836cdade16
Jon Combe <github@joncombe.net>
2019-06-01 01:58:40 +0000

    Loyalty bug fix

--- saleboxdjango/management/commands/saleboxsync.py
+++ saleboxdjango/management/commands/saleboxsync.py
@@ -1132,7 +1132,7 @@ class Command(BaseCommand):
 
             basket.append({
                 'discount_ruleset': None,
-                'loyalty': v['loyalty'],
+                'qty_loyalty': v['qty_loyalty'],
                 'price_original': v['qty_price'],
                 'price_modified': v['qty_sale_price'],
                 'product_weight': None,
