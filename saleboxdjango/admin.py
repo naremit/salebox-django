71631c9e62701686042ce26802155cdc3d3bbdfd
Jon Combe <github@joncombe.net>
2020-03-07 09:36:06 +0700

    Show salebox fields in user admin

--- saleboxdjango/admin.py
+++ saleboxdjango/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
+
 from saleboxdjango.models import *
 
 
@@ -15,7 +18,6 @@ class DiscountRulesetInline(admin.TabularInline):
     exclude = ['product_variant']
     extra = 0
 
-
 class AnalyticAdmin(admin.ModelAdmin):
     list_display = ('key', 'first_seen', 'last_seen', 'page_views', 'ip_country', 'ip_city', 'ip_address', 'ua_browser_family', 'ua_os_family', 'language', 'screen_size', 'ua_is_mobile', 'ua_is_tablet', 'ua_is_pc', 'ua_is_bot')
     readonly_fields = ('key', 'first_seen', 'last_seen', 'page_views', 'ip_address', 'ip_country', 'ip_city', 'ip_tz', 'ip_lat', 'ip_lng', 'ua_browser_family', 'ua_browser_version', 'ua_os_family', 'ua_os_version', 'ua_device_family', 'ua_device_brand','ua_device_model',  'ua_is_mobile', 'ua_is_tablet', 'ua_is_touch_capable', 'ua_is_pc', 'ua_is_bot', 'language', 'screen_width', 'screen_height')
@@ -29,22 +31,18 @@ class AnalyticAdmin(admin.ModelAdmin):
 class AttributeAdmin(admin.ModelAdmin):
     list_display = ('code',)
 
-
 class AttributeItemAdmin(admin.ModelAdmin):
     list_display = ('value', 'slug', 'attribute')
     list_filter = ('attribute',)
-
 
 class BasketWishlistAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'session', 'variant', 'quantity', 'weight', 'basket_flag')
     list_filter = ('basket_flag',)
     search_fields = ('user__email',)
 
-
 class CallbackStoreAdmin(admin.ModelAdmin):
     list_display = ('created', 'ip_address', 'method')
     search_fields = ('post',)
-
 
 class CheckoutStoreAdmin(admin.ModelAdmin):
     list_display = ('created', 'status', 'uuid', 'visible_id', 'user', 'gateway_code')
@@ -53,20 +51,16 @@ class CheckoutStoreAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid', 'visible_id', 'user', 'gateway_code', 'status', 'data', 'payment_received')
     search_fields = ('visible_id', 'data')
 
-
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('code', 'default')
 
-
 class CountryStateAdmin(admin.ModelAdmin):
     list_display = ('country', 'code', 'full_code')
-
 
 class DiscountGroupAdmin(admin.ModelAdmin):
     inlines = [DiscountRulesetInline]
     list_display = ('name', 'group_type')
     list_filter = ('group_type',)
-
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('event', 'salebox_member_id', 'transaction_guid', 'processed_flag', 'created')
@@ -74,14 +68,11 @@ class EventAdmin(admin.ModelAdmin):
     readonly_fields = ('event', 'salebox_member_id', 'transaction_guid', 'created')
     search_fields = ('salebox_member_id', 'transaction_guid')
 
-
 class LastUpdateAdmin(admin.ModelAdmin):
     list_display = ('code', 'value')
 
-
 class MemberGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'flat_discount_percentage')
-
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('guid', 'email', 'name_first', 'name_last', 'group')
@@ -89,27 +80,33 @@ class MemberAdmin(admin.ModelAdmin):
     readonly_fields = ('parent',)
     search_fields = ('guid', 'email')
 
-
 class ProductCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug_path')
     search_fields = ('name', 'slug_path')
-
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'sold_by', 'slug')
     list_filter = ('sold_by', 'category')
     search_fields = ('name', 'slug', 'plu', 'sku')
 
-
 class ProductVariantAdmin(admin.ModelAdmin):
     list_display = ('product', 'name', 'price', 'slug')
     list_filter = ('product__category',)
-
 
 class ProductVariantRatingAdmin(admin.ModelAdmin):
     list_display = ('user', 'variant', 'rating', 'created')
     search_fields = ('user__email',)
 
+class SaleboxUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        ('Salebox', { 'fields': ('salebox_member_id', 'salebox_member_sync')})
+    )
 
 class TranslationAdmin(admin.ModelAdmin):
     list_display = ('language_code', 'key', 'value')
@@ -157,3 +154,12 @@ admin.site.register(ProductVariant, ProductVariantAdmin)
 admin.site.register(ProductVariantRating, ProductVariantRatingAdmin)
 admin.site.register(Translation, TranslationAdmin)
 admin.site.register(UserAddress, UserAddressAdmin)
+
+"""
+
+from django.contrib.auth.admin import UserAdmin
+from saleboxdjango.admin import SaleboxUserAdmin
+
+admin.site.register(SaleboxUser, SaleboxUserAdmin)
+
+"""
